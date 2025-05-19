3fd6a64e5ebce486493c669c8c91b88eb335c3bd
Vineel Pynam <vineelpynam21@gmail.com>
2025-05-19 10:20:40 +0530

    addding iterator pattern cpp

---
+++ behavioural_patterns/c++/iterator/pattern.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+template<typename T>
+class Iterator{
+public:
+    virtual bool hasNext() = 0;
+    virtual T next() = 0;
+    virtual ~Iterator() = default;
+};
+
+template<typename T>
+class CustomList: public Iterator<T>{
+private:
+    vector<T> arr;
+    int size = 0;
+    int index = 0;
+public:
+    CustomList(const vector<T>& arr){
+        this->arr = arr;
+        this->size = arr.size();
+    }
+
+    T next() override {
+        if( !hasNext() ){
+            throw out_of_range("No more elements");
+        }
+        return arr[index++];
+    }
+
+    bool hasNext() override {
+        return index < size;
+    }
+};
+
+int main(){
+
+    vector<int> intArr = {1,2,3,4,5};
+    vector<string> stringArr = {"a", "b", "c", "d", "e", "f"};
+
+    Iterator<int>* intIt = new CustomList<int>(intArr);
+    while( intIt->hasNext() ){
+        cout << intIt->next() << " ";
+    }
+    cout << endl;
+    Iterator<string>* stringIt = new CustomList<string>(stringArr);
+    while( stringIt->hasNext() ){
+        cout << stringIt->next() << " ";
+    }
+
+    cout << endl;
+
+}
